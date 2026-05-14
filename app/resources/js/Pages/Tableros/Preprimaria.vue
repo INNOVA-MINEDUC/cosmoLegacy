70eb65678d76c76fc66a2ba1307e41933f88b4fd
Sincal200 <sincaljulio200@gmail.com>
2026-05-14 10:07:07 -0600

    Add conditional rendering for item links in Preprimaria and Primaria pages

--- app/resources/js/Pages/Tableros/Preprimaria.vue
+++ app/resources/js/Pages/Tableros/Preprimaria.vue
@@ -64,7 +64,7 @@ defineProps(['libros'])
                         </th>
                         <td class="px-6 py-4 text-center">
     <!--                        <a :href="item.link" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Ver</a>-->
-                            <a :href="item.enlace" target=”_blank” class="inline-flex items-center px-2.5 py-2.5 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                            <a v-if="item.enlace" :href="item.enlace" target="_blank" class="inline-flex items-center px-2.5 py-2.5 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                                 Ver
 <!--                                <svg class="w-3.5 h-3.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
--- app/resources/js/Pages/Tableros/Primaria.vue
+++ app/resources/js/Pages/Tableros/Primaria.vue
@@ -62,7 +62,7 @@ defineProps(['libros'])
                         {{ item.materia }}
                     </th>
                     <td class="px-6 py-4 text-center">
-                        <a :href="item.enlace" target=”_blank” class="inline-flex items-center px-2.5 py-2.5 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                        <a v-if="item.enlace" :href="item.enlace" target="_blank" class="inline-flex items-center px-2.5 py-2.5 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                             Ver
                         </a>
                     </td>
